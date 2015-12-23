47aa5a4a4e89f6ed7c10ff24b9240fcb8b1e89a8
DownToSky <mme94@scarletmail.rutgers.edu>
2015-12-23 18:17:32 -0500

    Smallest multiple
    not the fastest way still. REALLY SLOW in fact

--- Euler5.py
+++ Euler5.py
@@ -8,7 +8,7 @@ def divisbleByAll(n):
 #Brute Force:
 #If we find the biggest shared roorts of all number until 20 we can find it MUCH FASTER!
 #for example for 1-10: 8*9*5*7
-#and for 20 is: 16*9*5*7*11*13*17*19
+#and for 20 is: 16*9*5*7*11*13*17*19 (wich is also the answer to this euler problem)
 
 number = 1
 while number>0:
