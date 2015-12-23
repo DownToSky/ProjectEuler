0de7cd435547c585ec713c1333aa2b61d30e7eb8
DownToSky <mme94@scarletmail.rutgers.edu>
2015-12-23 18:22:10 -0500

    Sum square difference

--- Euler6.py
+++ Euler6.py
@@ -1,5 +1,9 @@
 #Author DownToSky
 #Euler Problem 6
+
+#Note that 1+2+...+n=n(n-1)/2
+#Using the fact that 1^2+2^2+...+n^2=n(n+1)(2n+1)/6 to
+#improve this solution
 firstSum =0
 secondSum =0
 for x in range(1, 101):
