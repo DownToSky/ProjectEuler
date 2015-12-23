79efa7cbb0b794838aa544699a9be620a24bc7d9
DownToSky <mme94@scarletmail.rutgers.edu>
2015-12-23 18:04:00 -0500

    Even Fibonacci numbers

--- Euler2.py
+++ Euler2.py
@@ -1,13 +1,28 @@
 #Author DownToSky
 #Euler Problem 2
-fibPointer1 = 1
-fibPointer2 = 1
-sum=0
+
+#Recursinve approach is not recommended for this problem since
+#it has to find the previous fib numbers when the next
+#fib number is going to be re-evaluated
+#We can define fibbonacci sequence by:
+#fib(1)=1
+#fib(2)=1
+#fib(i)=fib(i-1)+fib(i-2)
+#Hence, by storing the last two fibNumbers the next fibonaci number
+#can be found
+fibPointer1 = 1         #This is always the last fib number found: fib(i-1)
+fibPointer2 = 1         #This is always the second last fib number found: fib(i-2)
+sum=0                   #Contains the final answer
+
 while fibPointer1<4000000:
     if fibPointer1%2==0:
         sum= sum+fibPointer1
-        
-    nextFib = fibPointer1+fibPointer2
+    
+    #Finding the next fib number based one the last two:
+    nextFib = fibPointer1+fibPointer2   
+    
+    #Updating the fib(i-1) and fib(i-2):
     fibPointer2=fibPointer1
     fibPointer1=nextFib
+    
 print (str(sum))
