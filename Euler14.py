88e9a43da5f1da011cffb373a35545b62d11bdfd
DownToSky <mme94@scarletmail.rutgers.edu>
2015-12-24 16:11:29 -0500

    Longest Collatz sequence

--- Euler14.py
+++ Euler14.py
@@ -1,3 +1,6 @@
+#Author DownToSky
+#Euler Problem 14
+
 #returns the legth of the collatz sequence starting with number n
 def sequenceLength(n):
     count =0
@@ -8,10 +11,10 @@ def sequenceLength(n):
         else:
             n=3*n+1
             count+=1
-    #since we never count the last number that blongs to the sequence 1
+    #since we never count the last number that blongs to the sequence: 1
     count+=1
     return count
-    
+#Iterating through all numbers and finding the one with largest seq length    
 largestSq=0
 largestStart=1
 for i in range(1,1000000+1):
