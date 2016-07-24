f11731eb23fb7b9f1874e87702f85b6231392b71
DownToSky <mme94@scarletmail.rutgers.edu>
2016-07-24 13:52:28 -0400

    still wrong solution

--- Euler29.py
+++ Euler29.py
@@ -2,18 +2,41 @@
 #Euler Problem 29
 
 a_min=b_min=2
-a_max=b_max=100
+a_max=b_max=9
 
+def divisors(num,)
 
-#Making an arrey of tuples
-#The first element in the tuple is the base
-#and the second is a list of its valid exponents
-a=[]
-for a_i in range(0,a_max-a_min+1):  # +1 for inclusiveness
-    a.append((a_min+a_i,[]))
-    for b_i in range(0,b_max-b_min+1):
-        a[a_i][1].append(b_min+b_i)
+#Making a dictionary of bases and their exponents
+#The key is the base and the value of the key is a list of its valid exponents
 
-print(a)
+pairs=dict()
+for a in range(a_min,a_max+1):  # +1 for inclusiveness
+    pairs[a]=set()
+    for b in range(b_min,b_max+1):
+        pairs[a].add(b)
+#print(pairs)
+#Removing the duplicate base/exponent pairs
 
-    
+for a in range(a_min,a_max+1):
+    for b in range(b_min,b_max+1):
+        if a**b in pairs:
+            #print(str(a)+" "+str(b))
+            for bad_elem in range(1,int(b_max/b)+1):
+                #print("str:"+str(bad_elem))
+                pairs[a**b].discard(bad_elem)
+        
+            #Then the next b is not gonna make a^b exits in pairs
+            
+print("After removing duplicates:\n"+str(pairs))
+number_of_distinct_elems=0
+for a in pairs:
+    number_of_distinct_elems+=len(pairs[a])
+print("\n\nThe total number of distinct elements:\n"+str(number_of_distinct_elems))
+
+test_list=[]
+for a in pairs:
+    for b in pairs[a]:
+        if a**b not in test_list:
+            test_list.append(a**b)
+        else:
+            raise ValueError(str(a**b)+" already exists in pairs. a="+str(a)+" b="+str(b))
