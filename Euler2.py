c0c2297ef9f56e875c17479dd32f65d0bfe098b0
downtosky <mme94@scarletmail.rutgers.edu>
2016-10-07 14:46:34 -0400

    problem 122 and 104 done

--- Euler2.py
+++ Euler2.py
@@ -1,28 +1,13 @@
+from utils import fib
 #Author DownToSky
 #Euler Problem 2
 
-#Recursinve approach is not recommended for this problem since
-#it has to find the previous fib numbers when the next
-#fib number is going to be re-evaluated
-#We can define fibbonacci sequence by:
-#fib(1)=1
-#fib(2)=1
-#fib(i)=fib(i-1)+fib(i-2)
-#Hence, by storing the last two fibNumbers the next fibonaci number
-#can be found
-fibPointer1 = 1         #This is always the last fib number found: fib(i-1)
-fibPointer2 = 1         #This is always the second last fib number found: fib(i-2)
-sum=0                   #Contains the final answer
-
-while fibPointer1<4000000:
-    if fibPointer1%2==0:
-        sum= sum+fibPointer1
-    
-    #Finding the next fib number based one the last two:
-    nextFib = fibPointer1+fibPointer2   
-    
-    #Updating the fib(i-1) and fib(i-2):
-    fibPointer2=fibPointer1
-    fibPointer1=nextFib
-    
-print (str(sum))
+ans=0
+n=0
+f=fib._Fib1(n)
+while f<=4000000:
+	if f%2==0:
+		ans+=f
+	n+=1
+	f=fib._Fib1(n)
+print(ans)
